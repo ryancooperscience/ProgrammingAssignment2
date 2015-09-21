03c01c8223376d3fd91a9ef3f4efac28dd493fef
Ryan Cooper <rc2555@columbia.edu>
2015-09-21 07:36:34 -0400

    added solve functions to matrix makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix that is a list of values including the matrix
+## and the setSolve, getSolve, and set function to set matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  sol<-NULL
+  set <- function(y){
+    x<<-y
+    sol<<-NULL
+  }
+  get <- function(){
+    x
+  }
+  setSolve <- function(solve){
+    sol <<- solve
+  }
+  getSolve <- function() sol
+  list(set=set,get=get,setSolve=setSolve, getSolve=getSolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve caches the solution of a matrix into the getSolve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  sol<-x$getSolve()
+  if(!is.null(sol)){
+    message('getting cached solution')
+    return(sol)
+  }
+  data <- x$get()
+  sol <- solve(data, ...)
+  x$setSolve(sol)
+  sol 
 }
